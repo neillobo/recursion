b2df0e3e2d6e29caa000f789b37a58e9833c23d0
neillobo <neillobo@outlook.com>
2014-05-16 15:40:44 -0700

    My very own stringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,7 +1,63 @@
 // this is what you would do if you liked things to be easy:
-// var stringifyJSON = JSON.stringify;
-
+//var stringifyJSON = JSON.stringify;
 // but you don't so you're going to write it from scratch:
+
+Object.size = function(obj) {
+    var size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+   
+    if(obj===undefined) return "";
+     if (obj==null) return 'null';
+    
+    var type=typeof obj;
+    
+    var resultString='';
+    
+    switch(type){
+        case "function":
+            return "";
+        case "number":
+            return obj.toString();
+        case "boolean":
+            return obj.toString();
+        case "string":
+            return "\""+obj+"\"";
+        case 'object':
+            if (Array.isArray(obj)) {
+                if (obj.length===0) return "[]";
+                    //otherwise it is an array with elements
+                    resultString = "[";
+                    obj.forEach(function(item, index){
+
+                        resultString+= stringifyJSON(item);
+                        if(index!==obj.length-1)
+                        resultString+= ',';
+                    });
+            resultString+="]";
+            return resultString;
+            }
+            else{
+                //Now it is an object
+               var osize = Object.size(obj),i=0;
+               if (osize===0) return "{}"; 
+               resultString = "{";
+                    for (var key in obj){
+                    i++;
+                      if (stringifyJSON(obj[key])!==""){
+                        resultString+=stringifyJSON(key)+":"+stringifyJSON(obj[key]);
+                        if(i!==osize)
+                            resultString+=",";
+                      }
+                    }
+                    
+            resultString+="}";
+            return resultString;
+            }
+    }
 };
